d943ba493f6770f4cd2c22128680a9ce2491ccb0
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2022-12-29 03:29:28 -0800

    add 100 pdf file

--- ExamNew.java
+++ ExamNew.java
@@ -7,10 +7,13 @@ public class ExamNew {
 
 	public static void main(String[] args) {
 		try {
+			for(int i=0; i<=99; i++) {		
 			Document document = new Document();
-			PdfWriter.getInstance(document,new FileOutputStream("C:\\Users\\Lenovo\\eclipse-workspace\\ExamNew\\src\\test.pdf"));
+			PdfWriter.getInstance(document,new FileOutputStream("C:\\Users\\Lenovo\\eclipse-workspace\\ExamNew\\src\\pdfFile"+i+".pdf"));
+			document.open();
 			document.add(new Paragraph("Example"));
 			document.close();
+			}
 		}catch(Exception e){
 			System.out.println(e);
 		}
