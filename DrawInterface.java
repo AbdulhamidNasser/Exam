47c1602ce7ec21eae2610fe2f3cb1b41bfd64060
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2023-01-02 04:07:19 -0800

    use interface

---
+++ DrawInterface.java
@@ -0,0 +1,8 @@
+
+public class DrawInterface implements TestInterface {
+	
+	public void draw(){
+		System.out.println("Tray Drawing");
+		}  
+
+}
---
+++ MainInterface.java
@@ -0,0 +1,12 @@
+
+public class MainInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TestInterface interfaceobj = new DrawInterface();
+		interfaceobj.draw();
+
+	}
+
+}
